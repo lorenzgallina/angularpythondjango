83932ea1d04d3c61eca29b1646c02bd78a5f0fe2
lgallina <lgallina@student.ethz.ch>
2023-11-04 22:07:27 +0100

    starting implementing components for fitnesstracker

---
+++ angular/angular-app/src/app/fitnesstracker/exercise-list/exercise-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exercise-lst',
+  templateUrl: './exercise-list.component.html',
+  styleUrls: ['./exercise-list.component.css']
+})
+export class ExerciseLstComponent {
+
+}
---
+++ angular/angular-app/src/app/fitnesstracker/workout-plan/workout-plan.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-workout-plan',
+  templateUrl: './workout-plan.component.html',
+  styleUrls: ['./workout-plan.component.css']
+})
+export class WorkoutPlanComponent {
+
+}
