250eee89f59d867de883669087b0e8991244e8a8
lgallina <lgallina@student.ethz.ch>
2023-12-09 12:48:02 +0100

    small changes in frontend

--- angular/angular-app/src/app/fitnesstracker/workout-display/workout-display.component.ts
+++ angular/angular-app/src/app/fitnesstracker/workout-display/workout-display.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ExerciseLog, Workout, WorkoutPlan } from '../interfaces';
+import { Exercise, ExerciseLog, Workout, WorkoutPlan } from '../interfaces';
 import { ApiService } from '../fitness.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-workout-display',
@@ -13,11 +14,13 @@ export class WorkoutDisplayComponent implements OnInit {
   selectedWorkoutPlanId: number | undefined;
   workouts: Workout[] | undefined;
   exerciseLogs: { [key: number]: ExerciseLog[] } = {};
+  allExercises: Exercise[] | undefined;
 
-  constructor(private apiService: ApiService) {}
+  constructor(private apiService: ApiService, private snackBar: MatSnackBar) {}
 
   ngOnInit() {
     this.loadWorkoutPlans();
+    this.getExercises();
   }
 
   loadWorkoutPlans() {
@@ -26,9 +29,25 @@ export class WorkoutDisplayComponent implements OnInit {
         this.workoutPlans = workoutplans;
       },
       (error) => {
+        this.snackBar.open('Error loading workoutplans.', 'Close', { duration: 3000 });
         console.error('Error loading workoutplans:', error);
       }
     );
+  }
+
+  getExercises() {
+    this.apiService.getExercises().subscribe(
+      (exercises) => {
+        this.allExercises = exercises;
+      },
+      (error) => {
+        console.error('Error loading exercises:', error);
+      }
+    );
+  }
+
+  findExercise(exerciseId: number): Exercise | undefined {
+    return this.allExercises?.find(exercise => exercise.id === exerciseId);
   }
 
   onWorkoutPlanSelected() {
@@ -39,13 +58,16 @@ export class WorkoutDisplayComponent implements OnInit {
           this.apiService.getAllExerciseLogsGroupedByWorkout(this.selectedWorkoutPlanId).subscribe(
             (groupedExerciseLogs) => {
               this.exerciseLogs = groupedExerciseLogs;
+              this.snackBar.open('Exercise Logs Loaded successfully!', 'Close', { duration: 3000 });
             },
             (error) => {
+              this.snackBar.open('Error loading exerciseLogs.', 'Close', { duration: 3000 });
               console.error('Error loading exerciseLogs:', error);
             }
           );
         },
         (error) => {
+          this.snackBar.open('Error loading workouts.', 'Close', { duration: 3000 });
           console.error('Error loading workouts:', error);
         }
       );
