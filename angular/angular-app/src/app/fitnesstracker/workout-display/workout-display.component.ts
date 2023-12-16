88c7c027e9e07da6124d48e8c37a2315f494985d
lgallina <lgallina@student.ethz.ch>
2023-12-16 14:41:57 +0100

    added charts, first approach

--- angular/angular-app/src/app/fitnesstracker/workout-display/workout-display.component.ts
+++ angular/angular-app/src/app/fitnesstracker/workout-display/workout-display.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Exercise, ExerciseLog, Workout, WorkoutPlan } from '../interfaces';
 import { ApiService } from '../fitness.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ChartDataset, ChartOptions, ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-workout-display',
@@ -15,6 +16,51 @@ export class WorkoutDisplayComponent implements OnInit {
   workouts: Workout[] | undefined;
   exerciseLogs: { [key: number]: ExerciseLog[] } = {};
   allExercises: Exercise[] | undefined;
+
+  // chart:
+  public lineChartWeightData: ChartDataset[] = [{
+    data: [],
+    label: 'Weight',
+    fill: true,
+    backgroundColor: 'rgba(255, 99, 132, 0.25)',
+    borderColor: 'rgb(255, 99, 132)',
+    //pointBackgroundColor: 'blue',
+    //pointRadius: 5,
+    //pointHoverRadius: 7,
+    type: 'line'
+  }];
+
+  public lineChartRepsData: ChartDataset[] = [{
+    data: [],
+    label: 'Reps',
+    fill: true,
+    backgroundColor: 'rgba(255, 99, 132, 0.25)',
+    borderColor: 'rgb(255, 99, 132)',
+    //pointBackgroundColor: 'green',
+    //pointRadius: 5,
+    //pointHoverRadius: 7,
+    type: 'line'
+  }];
+
+  public lineChartSetsData: ChartDataset[] = [{
+    data: [],
+    label: 'Sets',
+    fill: true,
+    backgroundColor: 'rgba(255, 99, 132, 0.25)',
+    borderColor: 'rgb(255, 99, 132)',
+    //pointBackgroundColor: 'red',
+    //pointRadius: 5,
+    //pointHoverRadius: 7,
+    type: 'line'
+  }];
+  
+  public lineChartLabels: string[] = [];
+  public lineChartOptions: ChartOptions = {
+    responsive: true,
+    // additional chart options here
+  };
+  public lineChartType: ChartType = 'line';
+  selectedExerciseId: number | null = null;
 
   constructor(private apiService: ApiService, private snackBar: MatSnackBar) {}
 
@@ -72,6 +118,28 @@ export class WorkoutDisplayComponent implements OnInit {
         }
       );
     }
+  }
+
+  onExerciseSelected() {
+    this.lineChartWeightData[0].data = [];
+    this.lineChartRepsData[0].data = [];
+    this.lineChartSetsData[0].data = [];
+    this.lineChartLabels = [];
+  
+    this.workouts?.forEach(workout => {
+      const exerciseLogsForWorkout = this.exerciseLogs[workout.id];
+      if (exerciseLogsForWorkout) {
+        const exerciseLog = exerciseLogsForWorkout.find(log => log.exercise === this.selectedExerciseId);
+        if (exerciseLog) {
+          this.lineChartWeightData[0].data.push(exerciseLog.weight);
+          this.lineChartRepsData[0].data.push(exerciseLog.reps);
+          this.lineChartSetsData[0].data.push(exerciseLog.sets);
+          this.lineChartLabels.push(new Date(workout.date).toLocaleDateString());
+        }
+      }
+    });
+  
+    // Update or rerender the chart if needed
   }
 
 }
