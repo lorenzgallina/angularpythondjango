96f7f4bab243673656964722e7982439a02427e7
lgallina <lgallina@student.ethz.ch>
2023-12-26 19:41:30 +0100

    losing to colors and immages

--- angular/angular-app/tailwind.config.js
+++ angular/angular-app/tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#f5425d',  // Example primary color
+        secondary: '#ffed4a',  // Example secondary color
+        accent: '#e3342f',  // Example accent color
+        // Add other colors or shades as needed
+      },
+    },
   },
   plugins: [],
 }
